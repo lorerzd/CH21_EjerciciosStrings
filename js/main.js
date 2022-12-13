0d31a9d6641e5a89eaf65a6a45afcba2f58f7282
lorena <lore.rdz2802@gmail.com>
2022-12-12 21:50:50 -0600

    se solucionó el primer ejercicio con Strings

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 //Ejercicios Strings
+
+let nombre = "Abraham"
+nombre = nombre.toUpperCase();
+let letra ="";
+let contFinal =0;
+
+for (let index = 0; index< nombre.length; index++) {
+    console.log(nombre.charAt(index));
+    let contTemp=0;
+    for (let cont = index+1; cont < nombre.length; cont++) {
+        if (nombre.charAt(index) == nombre.charAt(cont)) 
+        contTemp++
+        
+    }//for cont
+    if (contTemp>contFinal) {
+        contFinal=contTemp
+        letra = nombre.charAt(index);
+        
+    }
+
+} //for index
+
+console.log(letra, contFinal);
